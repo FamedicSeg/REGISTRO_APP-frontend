bd6e0ae1867a12966d1c81a49d4f190dcb0412fc
SantiagoZ98 <dog.lion2298@outlook.com>
2026-09-14 08:59:35 -0500

    feat: :zap: New change in the Modules

--- src/pages/registro.jsx
+++ src/pages/registro.jsx
@@ -1803,7 +1803,7 @@ useEffect(() => {
     
     console.log("DEBUG: form.modulo =", form.modulo, "form.turno =", form.turno, "listaLideres =", listaLideres);
     
-    if ((form.modulo === "MODULO 1" || form.modulo === "MODULO 2" || form.modulo === "MODULO 3" || form.modulo === "MODULO 7" || form.modulo === "SELLADO" || form.modulo === "MODULO 10" || form.modulo === "BOTAS SIMPLES"|| form.modulo === "GPA" || form.modulo === "MODULO 7"|| form.modulo === "VARIOS 1") && form.turno && listaLideres.length > 0) {
+    if ((form.modulo === "MODULO 1" || form.modulo === "MODULO 2" || form.modulo === "MODULO 3" || form.modulo === "MODULO 4" || form.modulo === "MODULO 6" || form.modulo === "MODULO 7" || form.modulo === "MODULO 8" || form.modulo === "SELLADO" || form.modulo === "MODULO 10" || form.modulo === "BOTAS SIMPLES"|| form.modulo === "GPA" || form.modulo === "VARIOS 1" || form.modulo === "VARIOS 2") && form.turno && listaLideres.length > 0) {
       // Configuración especial para GPA
       let liderFiltrado = [];
       
@@ -1856,7 +1856,7 @@ useEffect(() => {
     
     console.log("DEBUG: form.modulo =", form.modulo, "form.turno =", form.turno, "listaSupervisores =", _supervisores);
     
-    if ((form.modulo === "MODULO 1" || form.modulo === "MODULO 2" || form.modulo === "MODULO 3" || form.modulo === "MODULO 7" || form.modulo === "SELLADO" || form.modulo === "MODULO 10" || form.modulo === "BOTAS SIMPLES"|| form.modulo === "GPA" || form.modulo === "VARIOS 1") && form.turno && _supervisores.length > 0) {
+    if ((form.modulo === "MODULO 1" || form.modulo === "MODULO 2" || form.modulo === "MODULO 3" || form.modulo === "MODULO 4" || form.modulo === "MODULO 6" || form.modulo === "MODULO 7" || form.modulo === "MODULO 8" || form.modulo === "SELLADO" || form.modulo === "MODULO 10" || form.modulo === "BOTAS SIMPLES"|| form.modulo === "GPA" || form.modulo === "VARIOS 1" || form.modulo === "VARIOS 2") && form.turno && _supervisores.length > 0) {
       // Configuración especial para GPA
       let supervisorFiltrado = [];
       
